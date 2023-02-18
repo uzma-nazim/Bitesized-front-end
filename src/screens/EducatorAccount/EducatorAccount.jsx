a0381c9aa05a309d7e1a8724993ab2a3d25458c9
MuhammadEjaz11 <mejaz4349@gmail.com>
2023-02-18 22:22:08 +0500

    done with responsive component

--- src/screens/EducatorAccount/EducatorAccount.jsx
+++ src/screens/EducatorAccount/EducatorAccount.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import EducatorAccountTabs from '../../components/EducatorAccountTabs/EducatorAccountTabs'
 import EducatorAccountTabs2 from '../../components/EducatorAccountTabs2/EducatorAccountTabs.2';
 import Header from '../../components/Header/Header';
+import './EducatorAccount.scss'
 
 
 export default function EducatorAccount() {
     return (
         <div>
             <Header educatorLogin />
-            <div className="container">
+            <div className="container content-hide">
 
-            {/* <EducatorAccountTabs /> */}
+            <EducatorAccountTabs />
+            
+            </div>
+            <div className="container component-display">
+
             <EducatorAccountTabs2 />
             </div>
         </div>
